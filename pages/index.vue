c63d249912d11f464da7b3110c6edda35b782487
kantanishio <kantanishio@mg-oxide.com>
2023-01-07 15:29:33 +0900

    w

--- pages/index.vue
+++ pages/index.vue
@@ -70,7 +70,67 @@
 
 
 
+<div class="bg-white py-6 sm:py-8 lg:py-12">
+  <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+    <!-- text - start -->
+    <div class="mb-10 md:mb-16">
+      <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Our competitive advantage</h2>
 
+      <p class="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>
+    </div>
+    <!-- text - end -->
+
+    <div class="grid sm:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-8">
+      <!-- feature - start -->
+      <div class="flex flex-col border rounded-lg p-4 md:p-6">
+        <h3 class="text-lg md:text-xl font-semibold mb-2">Growth</h3>
+        <p class="text-gray-500 mb-4">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
+        <a href="#" class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-bold transition duration-100 mt-auto">More</a>
+      </div>
+      <!-- feature - end -->
+
+      <!-- feature - start -->
+      <div class="flex flex-col border rounded-lg p-4 md:p-6">
+        <h3 class="text-lg md:text-xl font-semibold mb-2">Security</h3>
+        <p class="text-gray-500 mb-4">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
+        <a href="#" class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-bold transition duration-100 mt-auto">More</a>
+      </div>
+      <!-- feature - end -->
+
+      <!-- feature - start -->
+      <div class="flex flex-col border rounded-lg p-4 md:p-6">
+        <h3 class="text-lg md:text-xl font-semibold mb-2">Cloud</h3>
+        <p class="text-gray-500 mb-4">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
+        <a href="#" class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-bold transition duration-100 mt-auto">More</a>
+      </div>
+      <!-- feature - end -->
+
+      <!-- feature - start -->
+      <div class="flex flex-col border rounded-lg p-4 md:p-6">
+        <h3 class="text-lg md:text-xl font-semibold mb-2">Speed</h3>
+        <p class="text-gray-500 mb-4">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
+        <a href="#" class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-bold transition duration-100 mt-auto">More</a>
+      </div>
+      <!-- feature - end -->
+
+      <!-- feature - start -->
+      <div class="flex flex-col border rounded-lg p-4 md:p-6">
+        <h3 class="text-lg md:text-xl font-semibold mb-2">Support</h3>
+        <p class="text-gray-500 mb-4">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
+        <a href="#" class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-bold transition duration-100 mt-auto">More</a>
+      </div>
+      <!-- feature - end -->
+
+      <!-- feature - start -->
+      <div class="flex flex-col border rounded-lg p-4 md:p-6">
+        <h3 class="text-lg md:text-xl font-semibold mb-2">Dark Mode</h3>
+        <p class="text-gray-500 mb-4">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
+        <a href="#" class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-bold transition duration-100 mt-auto">More</a>
+      </div>
+      <!-- feature - end -->
+    </div>
+  </div>
+</div>
 
 
 
@@ -154,6 +214,35 @@
   </div>
 </div>
 
+
+<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div class="grid grid-cols-2 row-gap-8 md:grid-cols-4">
+    <div class="text-center md:border-r">
+      <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">144K</h6>
+      <p class="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base">
+        Downloads
+      </p>
+    </div>
+    <div class="text-center md:border-r">
+      <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">12.9K</h6>
+      <p class="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base">
+        Subscribers
+      </p>
+    </div>
+    <div class="text-center md:border-r">
+      <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">48.3K</h6>
+      <p class="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base">
+        Users
+      </p>
+    </div>
+    <div class="text-center">
+      <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">24.5K</h6>
+      <p class="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base">
+        Cookies
+      </p>
+    </div>
+  </div>
+</div>
 
 </div>
 </template>
