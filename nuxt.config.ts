a56649beee5d0f344622f9d333f0ae3c66d7738e
kantanishio <kantanishio@mg-oxide.com>
2024-08-05 17:02:38 +0900

    aaa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,19 @@
-
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default ({
   router: {
     base: '/',
   },
+
   app: {
     buildAssetsDir: '/_nuxt/',
     baseURL: '/',
   },
+
   target: 'static',
+
   generate: {
     dir: 'dist',
   },
 
   modules: ['@nuxtjs/tailwindcss'],
-
-})
+});
