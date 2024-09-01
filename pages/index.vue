b199f054b9e1004a6d60c5114c603e16b94b5b6c
kantanishio <kantanishio@mg-oxide.com>
2024-09-01 18:30:56 +0900

    a

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
 
 
-<div class="container-sm ml-10 mt-10 text-gray-500">
+<div class="container-sm ml-10 mt-10 text-gray-800">
 
 <p class="text-xl">Knata Nishio</p>
 <p><br><br></p>
 <p>080 3093 8738</p>
 <p><br></p>
 <p>kantanishio@mg-oxide.com</p>
-<p></p>
+<p><br><br></p>
+<p>
+</p>
 
 </div>
 
