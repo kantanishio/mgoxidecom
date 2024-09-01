2629f309933c2f39e6f3f22afa6abaadaba29039
kantanishio <kantanishio@mg-oxide.com>
2024-09-01 18:38:51 +0900

    l

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 
 <div class="container-sm ml-10 mt-10 text-gray-500">
 
-<p class="text-xl text-gray-500">
+<p class="text-l text-gray-500">
     Knata Nishio
 <br>
     TV, radio, video, websites, etc... 
